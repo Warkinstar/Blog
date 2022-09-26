a79dae16f582b69d051d74930e3b236274db9093
Ramil <Warkinstar@mail.ru>
2022-09-26 19:41:43 +0600

    CH1
    Installing Python
    • Creating a Python virtual environment
    • Installing Django
    • Creating and configuring a Django project
    • Building a Django application
    • Designing data models
    • Creating and applying model migrations
    • Creating an administration site for your models
    Working with QuerySets and model managers
    • Building views, templates, and URLs
    • Understanding the Django request/response cycle

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     # Local
-    "accounts.apps.AccountsConfig"
+    "accounts.apps.AccountsConfig",
+    "blog.apps.BlogConfig",
 ]
 
 MIDDLEWARE = [
